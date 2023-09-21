e802b0e0d2f5220989a52a24a6375f3d4717f794
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-09-21 20:55:28 +0100

    Kickstart

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="app">
+
+      {/* Homepage (search) */}
+      <Home></Home>
+      
+      {/* Searchpage */}
+
+    </div>
+  );
+}
+
+export default App;
